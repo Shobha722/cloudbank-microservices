01a0d8d59019425d5a4796a166ffbf8a976d48a1
Shobha722 <chimpireddyshobharani@gmail.com>
2026-07-14 21:49:34 +0530

    Add Home component for landing page

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,21 @@
+function Home(){
+
+return(
+
+<div className="container mt-5">
+
+<h1>Welcome to CloudBank</h1>
+
+<p>
+
+Enterprise Banking Platform powered by AWS, Kubernetes and FastAPI.
+
+</p>
+
+</div>
+
+);
+
+}
+
+export default Home;
